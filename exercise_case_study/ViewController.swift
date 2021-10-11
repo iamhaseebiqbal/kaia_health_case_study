03b88999508f0a17fea9de2294956f2b220d0998
Haseeb Iqbal <haseeb.iqbal@check24.de>
2021-10-11 10:12:18 +0200

    empty project created

---
+++ exercise_case_study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exercise_case_study
+//
+//  Created by Haseeb Iqbal on 11.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
